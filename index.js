fdc7212e3c9bb08c78042625e72bf2365f299c55
Yashkrit-Singh <139995167+Yashkrit-Singh@users.noreply.github.com>
2024-07-08 11:29:32 +0530

    ...

--- index.js
+++ index.js
@@ -154,6 +154,8 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render('error', { err });
 })
 
-app.listen(3000, () => {
-    console.log("LIstening on port number 3000");
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`LIstening on port number ${port}`);
 });
